4daeda8d2653c9943ab25f7205164ab8d4829c4c
Daniel Pryor <dpryor@parchment.com>
2018-06-15 09:08:06 -0600

    Fixing permissions and adding build.

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-docker run -it --rm -p 80:80 -p 443:443 -v $PWD/tmp:/usr/share/nginx/html/tmp/ test
+#!/bin/bash
+docker build -t docker-basicweb .
+echo "Running docker-basic web on port 80 and 443.. Ctrl-c to exit"
+docker run -it --rm -p 80:80 -p 443:443 -v $PWD/tmp:/usr/share/nginx/html/tmp/ docker-basicweb
